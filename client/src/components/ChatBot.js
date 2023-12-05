6332c7f139a0e85fbf4833691ceb0ec863b1f328
wailHAMLAOUI <w_hamlaoui@estin.dz>
2023-12-05 17:09:53 +0100

    update

--- client/src/components/ChatBot.js
+++ client/src/components/ChatBot.js
@@ -5,11 +5,11 @@ import Copyright from "./Copyright";
 import Chat from "./views/Chat";
 
 const ChatBot = () => {
-  const ItemStyle = {
-    backgroundColor: "#e0e0e0",
-    borderRadius: "10px",
-    borderBottom: "1px solid #46C2CB",
-  };
+  // const ItemStyle = {
+  //   backgroundColor: "#e0e0e0",
+  //   borderRadius: "10px",
+  //   borderBottom: "1px solid #46C2CB",
+  // };
   // .App-header {
   //   background-color: #282c34;
   //   min-height: 100vh;
@@ -20,12 +20,22 @@ const ChatBot = () => {
   //   font-size: calc(10px + 2vmin);
   //   color: white;
   // }
+  const ItemStyle = {
+    backgroundColor: "#262525",
+    borderRadius: "10px",
+    borderBottom: "1px solid #000000",
+    border: "1px solid #ccc",
+    boxShadow: "5px 5px 10px #888888",
+    color: "white",
+  };
   return (
     
-    <header className="App-header">
-      <h1>Chatbot</h1>
-      <Chat />
-    </header>
+    <Box style={ItemStyle} spacing={3}>
+      <header className="App-header">
+        <h1>Chatbot</h1>
+        <Chat />
+      </header>
+    </Box>
   
   );
 };
