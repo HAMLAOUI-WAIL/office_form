e26f3b8c7412ec38789a9d5b26736273bee64fe1
wailHAMLAOUI <w_hamlaoui@estin.dz>
2024-01-27 11:28:14 +0100

    update

--- server.js
+++ server.js
@@ -22,10 +22,9 @@ const app = express();
 const httpServer = http.createServer(app);
 const io = new Server(httpServer, {
   cors: {
-    origin: ["http://localhost:3000"],
+    origin: "*",
   },
 });
-
 mongoose.set('strictQuery', true);
 
 io.use(authSocket);
@@ -33,8 +32,7 @@ io.on("connection", (socket) => socketServer(socket));
 
 const PORT = process.env.PORT || 4000;
 mongoose
-  // .connect(process.env.MONGO_URI, {
-  .connect("mongodb+srv://whamlaoui:wail1234567890@cluster0.dkwnwgx.mongodb.net/?retryWrites=true&w=majority", {
+  .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
